5825ac94dc10d28e73fb936b6c6ba5acc4db3a7c
h223chen <h223chen@uwaterloo.ca>
2017-02-04 17:59:31 -0500

    able to read from csv file from root project directory

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
+"use strict";
 var express = require('express');
+var f = require('./global_functions.js');
+var fs = require('fs')
 var app = express();
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
+
+var lines = [];
+var init = function() {
+	fs.readFile('test.csv', 'utf8', function(err, data) {
+		if (err) {
+			return f.error(err);
+		}
+		
+		lines = data.split('\n');
+		if (lines.length > 0) {
+			f.info("imported " + lines.length + " lines successfully");
+		} else {
+			f.warn("file imported but 0 lines were read");
+		}
+	});
+};
+
+init();
+
+app.get('/', function(req, res) {
+	res.send('Hello World');
 })
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
+var server = app.listen(8081, "127.0.0.1", function() {
+	var host = server.address().address
+	var port = server.address().port
+
+	console.log("Server listening at http://%s:%s", host, port)
 })
