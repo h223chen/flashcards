4f91e7554e84f037b88561641ef99bd937ec475e
h223chen <h223chen@uwaterloo.ca>
2017-03-03 18:05:15 -0500

    FIXED IT

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -11,6 +11,9 @@ app.controller('mainCtrl', [
 		$scope.selection = [];
 		$scope.count = 10;
 		$scope.submitClicked = false;
+		
+		var COLUMN_DELIMITER = '|';
+		var EXTRA_DELIMITER = '?';
 
 		var randomizer = function(low, high){
 			return Math.trunc((Math.random()*(high-low))+low);
@@ -29,6 +32,73 @@ app.controller('mainCtrl', [
 					$scope.correct = res.data.results;
 				});
 		}
+		
+		var handleGetResponseData = function(data) {			
+			var columnTitles = data[0].split(COLUMN_DELIMITER);
+			console.log("data: " + data);
+			for (var i=1; i<$scope.count; i++) { // Generate questions
+				console.log(i);
+				var choices = [];
+				
+				var numColumns = data[i].split(COLUMN_DELIMITER).length; // number of columns in document
+								var questionType = randomizer(0, numColumns); //Determines reference column of description in question
+                var choiceType = randomizer(0, numColumns); //Determines reference column of choices                
+                var answerIndex = randomizer(0,5); //where answer will be placed in choices
+                
+                while (questionType === choiceType) { //make sure answers and choices are not of same type
+                    choiceType = randomizer(0, numColumns);
+                }
+
+                // Select answer
+                var questionIndex = randomizer(1, data.length-1); // index of which question to ask
+                var line = data[questionIndex].split(COLUMN_DELIMITER);
+                var answerString = "";
+                
+				if (choiceType == 0) {
+					var drugNames = line[choiceType].split(EXTRA_DELIMITER);
+					var selectedName = randomizer(0, drugNames.length - 1);
+					answerString = drugNames[selectedName];
+				} else {
+					answerString = line[choiceType];
+				}
+				
+				//Keep questionIndex information to generate question
+				var answerDrugName = selectedName; //answer location in drug name array
+
+				//Generate choices
+				for (var j=0; j<5; j++) { //loop to get 5 answer choices
+					var choice = "";
+					if (answerIndex==j) { // this choice is the correct answer
+						if (choiceType === 0){
+							choice = answerString;
+						} else {
+							choice = line[choiceType];
+						}
+					} else {
+						var index = randomizer(1, data.length-1);
+						choice = data[index].split(COLUMN_DELIMITER)[choiceType];
+					}
+					
+					while (choices.indexOf(choice) > -1) {
+						console.log("TEST");
+						var index = randomizer(1, data.length-1);
+						choice = data[index].split(COLUMN_DELIMITER)[choiceType];
+					}
+					
+					choices[j] = choice;
+				}
+			
+				var question = {
+					question: "Question " + (i) + ": Which of the following is the " + columnTitles[choiceType] + " that matches the following " + columnTitles[questionType] + ": " + data[answerIndex].split(COLUMN_DELIMITER)[questionType] + "?",
+					answerChoices: choices 
+				};				
+				console.log(answerIndex);
+
+				$scope.questions[i-1] = question;
+				$scope.answers[i-1] = answerIndex;
+				$scope.selection[i-1] = null;
+			}
+		}
 
 		$http.get('/api/get/OcularDrugs').then(function(res) {
 			console.log(res);
@@ -37,61 +107,7 @@ app.controller('mainCtrl', [
 				return;
 			}
 
-			for (var i=1; i< data.length; i++) {
-				var line = data[i].split(',');
-			}
-			
-			var colTitles = data[0].split('|');
-
-			for (var i=0;i<$scope.count;i++){ //Generate questions
-				var choices = [];
-				var ansType = randomizer(0, data[i].split('|').length - 1); //Determines reference column of description in question
-                var choiceType = randomizer(0, data[i].split('|').length - 1); //Determines reference column of choices
-                var answerloc = randomizer(0,5); //where answer will be placed in choice
-                while (ansType==choiceType) { //make sure answers and choices are not of same type
-                    choiceType = randomizer(0, data[i].split('|').length - 1);
-                }
-
-                //Select answer
-                var index = randomizer(1, data.length-1); //index of answer in data
-				if (choiceType==0){
-					var DrugNames = data[index].split('|')[choiceType].split('?');
-					var selectedName = randomizer(0, DrugNames.length - 1);
-					var ansString = DrugNames[selectedName];
-				}
-				else{
-					var ansString = data[index].split('|')[choiceType];
-					var selectedName = 0; //In case it is left undefined
-				}
-				//Keep index information to generate question
-				var answerIndex = index; //answer location in csv
-				var answerDrugName = selectedName; //answer location in drug name array
-
-				//Generate choices
-				for (var j=0; j<5; j++){ //loop to get 5 answer choices
-					if (answerloc==j){
-						if (choiceType==0){
-							ansString = data[answerIndex]
-						}
-					}
-					while (choices.indexOf(ansString) > -1) {
-						index = randomizer(1, data.length-1);
-						ansString = data[index].split('|')[choiceType];
-					}
-					choices[j] = ansString;
-				}
-			
-				var qNum = i+1
-				var question = {
-					question: "Question " + qNum + ": Which of the following is the " + colTitles[choiceType] + " that matches the following " + colTitles[ansType] + ": " + data[answerIndex].split('|')[ansType] + "?",
-					answerChoices: choices 
-				};				
-				console.log(answerIndex);
-
-				$scope.questions[i] = question;
-				$scope.answers[i] = answerloc;
-				$scope.selection[i] = null;
-			}
+			 handleGetResponseData(data);
 
 			console.log($scope.questions);
 			console.log($scope.answers);
